de61e17add5751d85adc1358d3d2c30543f676c2
cpu900 <cpu660@gmail.com>
2024-01-05 16:38:37 +0100

    Changes to be committed: 	modified:   2.py

--- 2.py
+++ 2.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 #läs in namn
-fnamn = input("Ange förnamn")
-enamn = input("Ange efternamn")
+fnamn = input("Ange förnamn\n")
+enamn = input("Ange efternamn\n")
 
-hela_namn = f"{fnamn} {enamn)"
+hela_namn = f"{fnamn} {enamn}"
 
 print("Ditt namn är:", hela_namn)
 
